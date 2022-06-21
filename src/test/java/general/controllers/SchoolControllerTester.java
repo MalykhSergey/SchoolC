a716c9f8a760c14cab4cf197f1f89aaaf328c5d3
MalykhSergey <sergeymalykhdm@gmail.com>
2022-06-21 20:11:12 +0600

    Was created test (SchoolControllerTester) for SchoolController

---
+++ src/test/java/general/controllers/SchoolControllerTester.java
@@ -0,0 +1,33 @@
+package general.controllers;
+
+import general.controllers.forms.SchoolForm;
+import general.services.SchoolService;
+import general.utils.CheckDataBoolAnswer;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.ui.ConcurrentModel;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+
+public class SchoolControllerTester {
+    SchoolService schoolService = Mockito.mock(SchoolService.class);
+    Model model = new ConcurrentModel();
+    SchoolForm schoolForm = Mockito.mock(SchoolForm.class);
+    SchoolController schoolController = new SchoolController(schoolService);
+
+    @Test
+    public void testAddSchoolPostOnCompleted() {
+        Mockito.when(schoolService.checkSchoolName(any())).thenReturn(new CheckDataBoolAnswer(true,""));
+        schoolController.addSchoolPost(schoolForm,model);
+        assertEquals(model.getAttribute("completed"), "Школа успешно добавлена");
+    }
+
+    @Test
+    public void testAddSchoolPostOnSchoolError(){
+        Mockito.when(schoolService.checkSchoolName(any())).thenReturn(new CheckDataBoolAnswer(false,"Error!"));
+        schoolController.addSchoolPost(schoolForm,model);
+        assertEquals(model.getAttribute("error"), "Error!");
+    }
+}
