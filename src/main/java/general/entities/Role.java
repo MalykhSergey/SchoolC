71ff6c68e7ef3fc6f7b107e60560c01799853e77
Sergey Malykh <sergeymalykhdm@gmail.com>
2022-08-14 23:53:31 +0600

    Role add method createUserByRole.

--- src/main/java/general/entities/Role.java
+++ src/main/java/general/entities/Role.java
@@ -14,6 +14,14 @@ public enum Role {
         return name;
     }
 
+    public User createUserByRole(String userName, String password, School school, SchoolClass schoolClass) {
+        return switch (this) {
+            case Student -> new Student(userName, password, school, schoolClass);
+            case Teacher -> new Teacher(userName, password, school);
+            default -> new User(userName, password, school, this);
+        };
+    }
+
     Role(String roleName) {
         this.name = roleName;
     }
