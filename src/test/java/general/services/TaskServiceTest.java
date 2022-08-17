d0f9d5712e4e30e713f087e89b93eb07d89fe76b
Sergey Malykh <sergeymalykhdm@gmail.com>
2022-08-17 14:32:14 +0600

    Create TaskServiceTest instead TaskAndAnswerControllerTester.

---
+++ src/test/java/general/services/TaskServiceTest.java
@@ -0,0 +1,75 @@
+package general.services;
+
+import general.entities.School;
+import general.entities.SchoolClass;
+import general.entities.Task;
+import general.entities.Teacher;
+import general.reposes.TaskRepos;
+import general.utils.Result;
+import general.utils.StringLengthConstants;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.sql.Timestamp;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TaskServiceTest {
+    TaskRepos taskRepos = Mockito.mock(TaskRepos.class);
+    TaskService taskService = new TaskService(taskRepos);
+
+    School school = new School("school");
+
+    SchoolClass schoolClass = new SchoolClass("class", 1, school);
+    SchoolClass otherClass = new SchoolClass("class", 1, school);
+    Teacher teacher = new Teacher("Teacher", "password", school);
+    StringBuilder nameBuilder = new StringBuilder();
+    StringBuilder bodyBuilder = new StringBuilder();
+
+    {
+        schoolClass.setId(1L);
+    }
+
+    {
+        otherClass.setId(2L);
+    }
+
+    {
+        teacher.addSchoolClass(schoolClass);
+    }
+
+    @Test
+    void createTask() {
+        nameBuilder.setLength(StringLengthConstants.TaskName.getMinLength() + 1);
+        bodyBuilder.setLength(StringLengthConstants.TaskBody.getMinLength() + 1);
+        assertEquals(taskService.createTask(nameBuilder.toString(), bodyBuilder.toString(), "2077-12-12T23:00", schoolClass, teacher), Result.Ok);
+        assertEquals(taskService.createTask(nameBuilder.toString(), bodyBuilder.toString(), "2077-Dec-12T23:00", schoolClass, teacher), Result.InvalidDate);
+        assertEquals(taskService.createTask(nameBuilder.toString(), bodyBuilder.toString(), "2077-12-12T23:00", otherClass, teacher), Result.InvalidClassName);
+    }
+
+    @Test
+    void validateTask() {
+        nameBuilder.setLength(StringLengthConstants.TaskName.getMinLength() + 1);
+        bodyBuilder.setLength(StringLengthConstants.TaskBody.getMinLength() + 1);
+        Task rightTask = new Task(nameBuilder.toString(), bodyBuilder.toString(), schoolClass, teacher, new Timestamp(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(2)));
+        nameBuilder.setLength(StringLengthConstants.TaskName.getMinLength() - 1);
+        Task tooShortNameTask = new Task(nameBuilder.toString(), bodyBuilder.toString(), schoolClass, teacher, new Timestamp(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(2)));
+        nameBuilder.setLength(StringLengthConstants.TaskName.getMinLength() + 1);
+        bodyBuilder.setLength(StringLengthConstants.TaskBody.getMinLength() - 1);
+        Task tooShortBodyTask = new Task(nameBuilder.toString(), bodyBuilder.toString(), schoolClass, teacher, new Timestamp(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(2)));
+        bodyBuilder.setLength(StringLengthConstants.TaskBody.getMinLength() + 1);
+        nameBuilder.setLength(StringLengthConstants.TaskName.getMaxLength() + 1);
+        Task tooLongNameTask = new Task(nameBuilder.toString(), bodyBuilder.toString(), schoolClass, teacher, new Timestamp(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(2)));
+        nameBuilder.setLength(StringLengthConstants.TaskName.getMinLength() + 1);
+        bodyBuilder.setLength(StringLengthConstants.TaskBody.getMaxLength() + 1);
+        Task tooLongBodyTask = new Task(nameBuilder.toString(), bodyBuilder.toString(), schoolClass, teacher, new Timestamp(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(2)));
+        assertEquals(taskService.validateTask(rightTask), Result.Ok);
+        assertEquals(taskService.validateTask(tooShortNameTask), Result.TooShortTaskName);
+        assertEquals(taskService.validateTask(tooShortBodyTask), Result.TooShortTaskBody);
+        assertEquals(taskService.validateTask(tooLongNameTask), Result.TooLongTaskName);
+        assertEquals(taskService.validateTask(tooLongBodyTask), Result.TooLongTaskBody);
+        rightTask.setTimeStamp(new Timestamp(System.currentTimeMillis()));
+        assertEquals(taskService.validateTask(rightTask), Result.InvalidDate);
+    }
+}
